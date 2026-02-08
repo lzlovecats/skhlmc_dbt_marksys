763c739cd6f9dbf96dc8fc134c44fe504501aadc
Linzin Leung <93505127+lzlovecats@users.noreply.github.com>
2026-02-08 23:15:22 +0800

    changed name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.set_page_config(page_title="電子分紙系統（Beta）", layout="wide")
+st.set_page_config(page_title="聖呂中辯電子分紙系統", layout="wide")
 
 page_judging = st.Page("judging.py", title="電子分紙（評判用）")
 page_match_mgmt = st.Page("match_info.py", title="賽事資料管理系統（賽會人員用）")
