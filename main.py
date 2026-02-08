7161d8ae07b8d35f7771080b5f3150d6ffe061ea
Linzin Leung <93505127+lzlovecats@users.noreply.github.com>
2026-02-08 21:37:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-page_match_mgmt = st.Page("match_info.py", title="賽事資料管理系統（賽會人員用）")
 page_judging = st.Page("judging.py", title="電子分紙（評判用）")
+page_match_mgmt = st.Page("match_info.py", title="賽事資料管理系統（賽會人員用）")
 page_mgmt = st.Page("management.py", title="分數管理（賽會人員用）")
 
 pg = st.navigation([page_judging, page_match_mgmt, page_mgmt])
