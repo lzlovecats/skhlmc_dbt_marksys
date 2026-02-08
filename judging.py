30ab64616048d33b4371ded6b77c9829aa6c5065
Linzin Leung <93505127+lzlovecats@users.noreply.github.com>
2026-02-08 22:00:14 +0800

    Update judging.py

--- judging.py
+++ judging.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import gspread
-from google.oauth2.service_account import Credentials
 from datetime import datetime
 from functions import load_data_from_gsheet, get_connection
 
