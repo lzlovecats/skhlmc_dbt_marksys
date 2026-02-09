7747d30b0fe4cacd4374e36f1b881938e121d2f2
Linzin Leung <93505127+lzlovecats@users.noreply.github.com>
2026-02-09 13:53:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ st.set_page_config(page_title="聖呂中辯電子分紙系統", layout="wide")
 page_judging = st.Page("judging.py", title="電子分紙（評判用）")
 page_match_mgmt = st.Page("match_info.py", title="賽事資料管理系統（賽會人員用）")
 page_mgmt = st.Page("management.py", title="分數管理（賽會人員用）")
+page_score_sheet = st.Page("review.py", title="查閱比賽分紙")
 
-pg = st.navigation([page_judging, page_match_mgmt, page_mgmt])
+pg = st.navigation([page_judging, page_match_mgmt, page_mgmt, page_score_sheet])
 
 if st.session_state.get("admin_logged_in"):
     with st.sidebar:
