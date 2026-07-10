45f5cc42771f9bff2d09b351a4694b49d1338ea3
Linzin Leung <93505127+lzlovecats@users.noreply.github.com>
2026-07-10 11:11:10 +0800

    3.7.14 bug fix 1

--- deploy/proxy.py
+++ deploy/proxy.py
@@ -52,8 +52,8 @@ PWA_HEAD = """
 <link rel="apple-touch-icon" href="/app-icon-180.png">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-title" content="聖呂中辯">
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta name="theme-color" content="#111827">
 <style>
 input, textarea, select {
